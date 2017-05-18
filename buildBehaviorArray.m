1b6dcf61df689a44d33b13d353a4124c29009ea3
jkim0731 <jkimkj@gmail.com>
2017-05-18 12:55:29 -0700

    build behavior array

--- buildBehaviorArray.m
+++ buildBehaviorArray.m
@@ -1,6 +1,7 @@
 mice = {'AH0648', 'AH0650', 'AH0651', 'AH0652', 'AH0653'};
 base_dir = 'Z:\Data\2p\soloData\';
-for i = 1 : length(mice)
+% for i = 1 : length(mice)
+for i = 5
     d = [base_dir mice{i} '\'];
     cd(d)    
 %     merge_error_dir
@@ -16,6 +17,16 @@ for i = 1 : length(mice)
             end
             b{j} = Solo.BehavTrial2padArray(flist(j).name, session_name);
         end    
+    elseif i == 2 % for AH0650, S12 is lost.
+        for j = 1 : length(flist)
+            if j < 13
+                session_name = sprintf('S%02d',j-1);
+            else
+                session_name = sprintf('S%02d',j); 
+            end
+            b{j} = Solo.BehavTrial2padArray(flist(j).name, session_name);
+        end
+        
     else
         for j = 1 : length(flist)
             session_name = sprintf('S%02d',j-1);
