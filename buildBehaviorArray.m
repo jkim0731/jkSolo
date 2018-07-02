9b33b9d29a525267ef35a51fba3713c6875ce868
Phillip Maire <maire@usc.edu>
2018-07-02 16:51:17 -0700

    180702

--- buildBehaviorArray.m
+++ buildBehaviorArray.m
@@ -1,15 +1,11 @@
 
-mice = {'JK025','JK027','JK030','JK036','JK037','JK038','JK039','JK041'};
-% mice = {'JK036','JK037','JK038','JK039','JK041'};
-% mice = {'JK036','JK039'};
-base_dir = 'E:\SoloData\';
+mice = {'JK052','JK053','JK054','JK056'};
+base_dir = 'Y:\Whiskernas\JK_temp\SoloData\';
+merge_error_saved_bysessionnumber_mice(base_dir,mice)
 
-
-for i = 4 : length(mice)
-% for i = 4
+for i = 1 : length(mice)
     d = [base_dir mice{i} '\'];
     cd(d)    
-%     merge_error_saved_bysessionnumber_mice(base_dir,mice)
     flist = dir('data_JK*.mat');
     
     b = cell(1,length(flist));
--- merge_error_saved_bysessionnumber_mice.m
+++ merge_error_saved_bysessionnumber_mice.m
@@ -2,7 +2,7 @@ function merge_error_saved_bysessionnumber_mice(base_dir,mice)
 
 % input settings
 % base_dir = 'Y:\Whiskernas\JK_temp\SoloData\';
-mice = {'JK036','JK037','JK038','JK039','JK041'};
+% mice = {'JK036','JK037','JK038','JK039','JK041'};
 % mice = {'JK030'};
 max_n_pre = 2; % maximum # of pre sessions in these mice
 max_n_s = 40; % maximum # of training sessions in these mice
